5268573e9fc84c29c50d349b4eecfb2818f17da1
ZwellyTheKidd <zwelly.thekidd@outlook.com>
2023-12-08 16:09:12 +0200

    modified the js

--- chatbot.js
+++ chatbot.js
@@ -1,18 +1,33 @@
-function talk(){
+
+function talk() {
     var know = {
-        "Hello": "Hi there &#128540",
-    "Who are you" : "Hello, Alice  here ",
-    "How are you" : "Good :)",
-    "What can I do for you" : "Please Give us A Follow & Like.",
-    "Your followers" : "I have my family of 5000 members, i don't have follower ,have supportive Famiy  ",
-    "ok" : "Thank You So Much ",
-    "Bye" : "Okay! Will meet soon.."
+        "hello": "Hi there &#128540",
+        "who are you": "Hello, Alice here",
+        "how are you": "Good :)",
+        "what can I do for you": "Please give us a follow & like.",
+        "your followers": "I have my family of 5000 members, I don't have followers, have a supportive family",
+        "ok": "Thank you so much",
+        "bye": "Okay! Will meet soon.."
     };
-    var user = document.getElementById('userBox').value;
+    var user = document.getElementById('userBox').value.toLowerCase(); // Convert user input to lowercase
     document.getElementById('chatLog').innerHTML = user + "<br>";
+
     if (user in know) {
-    document.getElementById('chatLog').innerHTML = know[user] + "<br>";
-    }else{
-    document.getElementById('chatLog').innerHTML = "Sorry,I didn't understand <br>";
+        document.getElementById('chatLog').innerHTML = know[user] + "<br>";
+    } else {
+        document.getElementById('chatLog').innerHTML = "Sorry, I didn't understand <br>";
     }
-    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
